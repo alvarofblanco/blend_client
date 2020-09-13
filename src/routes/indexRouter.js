7a7a081bdf3dbba83ce5239b9d810f7e19e3923a
Alvaro Blanco <alvarofblanco@gmail.com>
2020-09-13 15:39:23 -0400

    mega menu updated

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -4,6 +4,5 @@ const indexController = require('../controllers/indexController');
 const indexRouter = express.Router();
 
 indexRouter.route('/').get(indexController.getIndex);
-indexRouter.route('/testing').get(indexController.getTesting);
 
 module.exports = indexRouter;
